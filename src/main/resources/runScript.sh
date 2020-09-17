2ff04dbeb0150756b9d1ab2c4eee23d4ca5918b5
Mayur <mayur.raleraskar@oracle.com>
2020-09-17 09:12:58 -0700

    latest commit Thu Sep 17 09:12:58 PDT 2020

--- src/main/resources/runScript.sh
+++ src/main/resources/runScript.sh
@@ -1,8 +1,8 @@
 # source ~/.bash_profile
 cd /Users/mraleras/SpringBootOciCustomLogsAndMonitoring/SpringBootOciCustomLogsAndMonitoring
 lazygit "latest commit $(date)"
-rext 158.101.3.137
-'
+ossh 158.101.3.137
+
 cd ~
 
 pkill -f "java"
@@ -11,12 +11,13 @@ rm -rf SpringBootOciCustomLogsAndMonitoring
 
 git clone https://github.com/mayur-oci/SpringBootOciCustomLogsAndMonitoring.git
 
-time 5
+time 3
 
 nohup mvn -f /home/opc/SpringBootOciCustomLogsAndMonitoring/pom.xml spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005" > ~/appTail.log&
-'
 
-rext 158.101.3.137 "bash"
+tail -f ~/appTail.log
+
+
 
 
 
