9fadd13fcbb1c7874c178e586f54062afa6cb239
Mayur <mayur.raleraskar@oracle.com>
2020-09-17 17:23:26 -0700

    latest commit Thu Sep 17 17:23:26 PDT 2020

--- src/main/java/org/example/ConfigHolder.java
+++ src/main/java/org/example/ConfigHolder.java
@@ -11,7 +11,7 @@ public class ConfigHolder {
     public static String monitoringProfileName = "MAYUR_ADMIN_PHX";
 
     static public String monitoringServiceEndpoint = "https://telemetry-ingestion.us-phoenix-1.oraclecloud.com/";
-    static public String monitoringCompartmentId = "ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq";
+    static public String monitoringCompartmentId = "ocid1.compartment.oc1..aaaaaaaaks6tggapluiw5egbylax2voxnga6e7qtt2ssyapnl4u3p2chydya";
     static public Region monitoringRegion = Region.US_PHOENIX_1;
 
     public static String namespace = "test_custom_compute_namespace";
